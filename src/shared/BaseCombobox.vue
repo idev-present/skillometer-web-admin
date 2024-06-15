e199af02f4ff2751fb10ff61053a669341aaa17b
Igor Zhuravlev <igor.zhuravlev@idev-present.com>
2024-06-15 14:47:34 +0300

    fix(app): vacancy from

--- src/shared/BaseCombobox.vue
+++ src/shared/BaseCombobox.vue
@@ -8,11 +8,13 @@
     >
       <ComboboxLabel class="block text-sm font-medium leading-6 text-gray-900">{{ label }}</ComboboxLabel>
       <div class="relative">
-        <ComboboxInput
-          class="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          @change="query = $event.target.value" @blur="query = ''" :display-value="(item) => item?.key" />
-        <ComboboxButton class="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
-          <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+        <ComboboxButton class="relative w-full">
+          <ComboboxInput
+            class="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-8 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            @change="query = $event.target.value" @blur="query = ''" :display-value="(item) => item?.key" />
+          <div class="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+            <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+          </div>
         </ComboboxButton>
 
         <ComboboxOptions v-if="filteredData.length > 0"
--- src/widgets/VacancyForm.vue
+++ src/widgets/VacancyForm.vue
@@ -3,8 +3,7 @@
     <div class="space-y-12">
       <div class="border-b border-gray-900/10 pb-12">
         <h2 class="text-base font-semibold leading-7 text-gray-900">{{ title }}</h2>
-        <p class="mt-1 text-sm leading-6 text-gray-600">This information will be displayed publicly so be careful what
-          you share.</p>
+        <p class="mt-1 text-sm leading-6 text-gray-600">Эта информация будет отображаться публично, поэтому будьте осторожны с тем, чем делитесь.</p>
 
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div class="col-span-4">
@@ -100,19 +99,18 @@
           </div>
 
           <div class="sm:col-span-2">
-            <BaseSelect
+            <BaseCombobox
               label="Специализация"
               :items="divisionList"
               v-model="division"
               :message="errors?.divisionId"
               @on-focus="clearError('divisionId')"
-
             />
           </div>
 
           <div class="sm:col-span-2 mt-8">
             <BaseCheckbox
-              label="Удаленно"
+              label="Удаленная работа"
               v-model="isRemote"
             />
           </div>
