eb4e933ba6d7259919db3b94cbf79797ff1d78eb
Igor Zhuravlev <igor.zhuravlev@idev-present.com>
2024-06-23 20:34:18 +0300

    feat(app): publish hh and habr

--- src/pages/VacancyView.vue
+++ src/pages/VacancyView.vue
@@ -32,6 +32,49 @@
           </div>
         </div>
         <div class="mt-5 flex xl:ml-4 xl:mt-0">
+          <div class="hidden sm:block mr-3">
+            <Menu as="div" class="relative inline-block text-left">
+            <div>
+              <MenuButton class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                Опубликовать в
+                <ChevronDownIcon class="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+              </MenuButton>
+            </div>
+
+            <transition
+              enter-active-class="transition ease-out duration-100"
+              enter-from-class="transform opacity-0 scale-95"
+              enter-to-class="transform opacity-100 scale-100"
+              leave-active-class="transition ease-in duration-75"
+              leave-from-class="transform opacity-100 scale-100"
+              leave-to-class="transform opacity-0 scale-95">
+              <MenuItems class="absolute right-0 z-40 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div class="py-1">
+                  <MenuItem v-slot="{ active }">
+                    <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2 text-sm']">
+                      <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/7/79/HeadHunter_logo.png"
+                        alt="hh-icon"
+                        class="mr-3 h-5 w-5"
+                      />
+                      HeadHunter
+                    </a>
+                  </MenuItem>
+                  <MenuItem v-slot="{ active }">
+                    <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2 text-sm']">
+                      <img
+                        src="https://amonitoring.ru/upload/iblock/57f/57f6eb229b4c06aa42ab2e8b2a279d7a.svg"
+                        alt="habr-icon"
+                        class="mr-3 h-5 w-5"
+                      />
+                      Хабр
+                    </a>
+                  </MenuItem>
+                </div>
+              </MenuItems>
+            </transition>
+          </Menu>
+          </div>
           <span class="hidden sm:block">
             <RouterLink
               :to="`/vacancies/edit/${data?.id}`"
